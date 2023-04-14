d2f5e3c624dd0d3b8cd688fe522a03ab36906c08
RIDDHI SHIVHARE <anshu26021973@gmail.com>
2023-04-14 16:47:49 +0530

    update

--- app.py
+++ app.py
@@ -4,26 +4,108 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
 import re
 import shutil
-import bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy  import SQLAlchemy
+from flask_login import  UserMixin
 
 
 #### Defining Flask App
 app = Flask(__name__)
 
+db_path = os.path.join(os.path.dirname(__file__), 'healthcare.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SECRET_KEY'] = 'helloworld'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db=SQLAlchemy(app)
 
-app.secret_key = 'your secret key'
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'alapk26021973@'
-app.config['MYSQL_DB'] = 'healthcare'
- 
-mysql = MySQL(app)
 
+
+class userregister(UserMixin, db.Model):
+    Patient_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    gender=db.Column(db.String(50))
+    DOB=db.Column(db.String, nullable=False)
+    phn=db.Column(db.String(50))
+    addr=db.Column(db.String(80))
+    bldgrp=db.Column(db.String(15))
+    email = db.Column(db.String(50))
+    psw = db.Column(db.String(80))
+    ename=db.Column(db.String(50))
+    relation=db.Column(db.String(15))
+    phone=db.Column(db.String(50))
+
+    def __init__(self, name,gender,dob,phn,addr,bldgrp,email,hashed_psw,ename,relation,phone):
+        self.name=name
+        self.gender=gender
+        self.DOB=dob
+        self.phn=phn
+        self.addr=addr
+        self.bldgrp=bldgrp
+        self.email=email
+        self.psw=hashed_psw
+        self.ename=ename
+        self.relation=relation
+        self.phone=phone
+
+class hospitalregister(db.Model):
+
+        hid = db.Column(db.Integer, primary_key=True,autoincrement=True)
+        name = db.Column(db.String(40))
+        psw = db.Column(db.String(200))
+        loc = db.Column(db.String(50))
+        area= db.Column(db.String(50))
+        pin = db.Column(db.String(50))
+        state = db.Column(db.String(50))
+        city = db.Column(db.String(50))
+
+        def __init__(self, name, hashed_psw, loc,area,pin,state,city):
+            self.name = name
+            self.loc = loc
+            self.area = area
+            self.pin = pin
+            self.state = state
+            self.city = city
+            self.psw = hashed_psw
+
+class policeregister(db.Model):
+    pid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(40))
+    psw = db.Column(db.String(200))
+    loc = db.Column(db.String(50))
+    area = db.Column(db.String(50))
+    pin = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    city = db.Column(db.String(50))
+
+    def __init__(self, name, hashed_psw, loc, area, pin, state, city):
+        self.name = name
+        self.loc = loc
+        self.area = area
+        self.pin = pin
+        self.state = state
+        self.city = city
+        self.psw = hashed_psw
+
+class medical_reports(db.Model):
+    mid=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, db.ForeignKey('userregister.Patient_id'), nullable=False,primary_key=False)
+    Dname = db.Column(db.String(40))
+    date = db.Column(db.String, nullable=False)
+    sym = db.Column(db.String(200))
+    doc = db.Column(db.String(50))
+    allergies = db.Column(db.String(50))
+    reports = db.Column(db.String(250))
+
+    def __init__(self, id,Dname, date, sym,Doc, allergies,file):
+        self.id = id
+        self.Dname = Dname
+        self.date = date
+        self.sym = sym
+        self.doc = Doc
+        self.allergies = allergies
+        self.reports = file
 
 @app.route('/')
 @app.route('/login', methods =['GET', 'POST'])
@@ -34,47 +116,45 @@ def login():
         username = request.form['username']
         password = request.form['password']
         # encoding user password
-        userBytes = password.encode('utf-8')
-        print(userBytes)
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM userregister WHERE name = % s' , (username, ))
-        account = cursor.fetchone()
-        if account:
-            result=bcrypt.checkpw(userBytes, account['psw'].encode('utf-8'))
-        cursor.execute('SELECT * FROM hospitalregister WHERE name = % s ', (username,))
-        account1=cursor.fetchone()
-        if account1:
-            result=bcrypt.checkpw(userBytes, account1['psw'].encode('utf-8'))
 
-        cursor.execute('SELECT * FROM policeregister WHERE name = % s ', (username,))
-        account2 = cursor.fetchone()
-        if account2:
-            result=bcrypt.checkpw(userBytes, account2['psw'].encode('utf-8'))
+        user = userregister.query.filter_by(name=username).first()
+        if user:
+            result=check_password_hash(user.psw, password)
+        hospital = hospitalregister.query.filter_by(name=username).first()
+        print(hospital)
+        if hospital:
+            result=check_password_hash(hospital.psw, password)
 
-        if account and result :
+        police = policeregister.query.filter_by(name=username).first()
+        if police:
+            result=check_password_hash(police.psw, password)
+
+        if user and result :
             session['loggedinu'] = True
-            session['id'] = account['Patient_id']
-            session['username'] = account['name']
-            session['phn']=account['phn']
+            session['id'] = user.Patient_id
+            session['username'] = user.name
+            session['phn']=user.phn
             msg = 'Logged in successfully !'
 
-            row_details = display_details(account['phn'])
-            row_records = display_reports(account['Patient_id'])
+            row_details = display_details(user.phn)
+            row_records = display_reports(user.Patient_id)
             print(row_records)
 
             return render_template('index_user.html', msg = msg, rows =row_records , row1=row_details)
-        elif account1 and result:
+        elif hospital and result:
             session['loggedinh'] = True
-            session['hid'] = account1['hid']
-            session['username'] = account1['name']
+            print(hospital.hid)
+            print(hospital.name)
+            session['hid'] = hospital.hid
+            session['username'] = hospital.name
             msg = 'Logged in successfully !'
             row=hospital_details(session['hid'])
             return render_template('index_hospital.html', msg=msg,row=row)
-        elif account2 and result:
+        elif police and result:
             session['loggedinp'] = True
-            session['pid'] = account2['pid']
-            session['username'] = account2['name']
+            session['pid'] = police.pid
+            session['username'] = police.name
             msg = 'Logged in successfully !'
             row = police_details(session['pid'])
             return render_template('index_police.html', msg=msg, row=row)
@@ -101,21 +181,22 @@ def registerh():
         pin = request.form['pin']
         state = request.form['state']
         city = request.form['city']
-        bytes = psw.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed_psw = bcrypt.hashpw(bytes, salt)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM hospitalregister WHERE name = % s', (name, ))
-        account = cursor.fetchone()
+
+        hashed_psw = generate_password_hash(psw, method='sha256')
+
+
+        account=hospitalregister.query.filter_by(name=name).all()
+        print(account)
         if account:
             msg = 'Account already exists !'
         elif not name or not psw :
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO hospitalregister VALUES (NULL, % s, % s, % s, % s, % s, % s, % s)', (name, hashed_psw, loc,area,pin,state,city ,))
-            mysql.connection.commit()
+            new_user=hospitalregister(name, hashed_psw, loc,area,pin,state,city)
+            db.session.add(new_user)
+            db.session.commit()
             msg = 'You have successfully registered !'
-            return render_template('login.html')
+            return render_template('login.html',msg=msg)
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('registerh.html', msg = msg)
@@ -131,21 +212,19 @@ def registerp():
         pin = request.form['pin']
         state = request.form['state']
         city = request.form['city']
-        bytes = psw.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed_psw = bcrypt.hashpw(bytes, salt)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM policeregister WHERE name = % s', (name, ))
-        account = cursor.fetchone()
+        hashed_psw = generate_password_hash(psw, method='sha256')
+
+        account=policeregister.query.filter_by(name=name).all()
         if account:
             msg = 'Account already exists !'
         elif not name or not psw :
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO policeregister VALUES (NULL, % s, % s, % s, % s, % s, % s, % s)', (name, hashed_psw, loc,area,pin,state,city ,))
-            mysql.connection.commit()
+            new_user = policeregister(name, hashed_psw, loc, area, pin, state, city)
+            db.session.add(new_user)
+            db.session.commit()
             msg = 'You have successfully registered !'
-            return render_template('login.html')
+            return render_template('login.html',msg=msg)
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('registerp.html', msg = msg)
@@ -165,12 +244,8 @@ def registeru():
         ename=request.form['ename']
         relation =request.form['relation']
         phone=request.form['phone']
-        bytes = psw.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hashed_psw = bcrypt.hashpw(bytes, salt)
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM userregister WHERE name = % s', (name, ))
-        account = cursor.fetchone()
+        hashed_psw = generate_password_hash(psw, method='sha256')
+        account = userregister.query.filter_by(name=name).all()
         if account:
             msg = 'Account already exists !'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
@@ -178,8 +253,9 @@ def registeru():
         elif not name or not psw or not email:
             msg = 'Please fill out the form !'
         else:
-            cursor.execute('INSERT INTO userregister VALUES (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s, % s)', (name,gender,dob,phn,addr,bldgrp,email,hashed_psw,ename,relation,phone, ))
-            mysql.connection.commit()
+            new_user = userregister(name,gender,dob,phn,addr,bldgrp,email,hashed_psw,ename,relation,phone)
+            db.session.add(new_user)
+            db.session.commit()
             msg = 'You have successfully registered ! Now Register your Face'
             return render_template('register_face.html', msg=msg)
     elif request.method == 'POST':
@@ -197,10 +273,10 @@ def registerm():
         allergies= request.form['allergies']
         file = request.form['reports']
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('INSERT INTO medical_records VALUES (% s, % s, % s, % s, % s, % s, %s)',
-                       (session['id'],Dname, date, sym,Doc, allergies,file))
-        mysql.connection.commit()
+        report = medical_reports(session['id'],Dname, date, sym,Doc, allergies,file)
+        db.session.add(report)
+        db.session.commit()
+
         msg = 'You have successfully uploaded the report ! '
         row_details = display_details(session['phn'])
         # print(account['Patient_id'])
@@ -214,27 +290,30 @@ def update():
     msg = ''
     if request.method == 'POST':
       id = session['id']
-      name = request.form['name']
-      gender = request.form['gender']
-      dob = request.form['DOB']
-      phn = request.form['phn']
-      addr = request.form['addr']
-      bldgrp = request.form['bldgrp']
-      email = request.form['email']
+      user = userregister.query.filter_by(Patient_id=id).first()
+      print(user)
+      user.name = request.form['name']
+      user.gender = request.form['gender']
+      user.dob = request.form['DOB']
+      user.phn = request.form['phn']
+      user.addr = request.form['addr']
+      user.bldgrp = request.form['bldgrp']
+      user.email = request.form['email']
+      user.psw = request.form['psw']
+      user.ename = request.form['ename']
+      user.relation = request.form['relation']
+      user.phone = request.form['phone']
       psw = request.form['psw']
-      ename = request.form['ename']
-      relation = request.form['relation']
-      phone = request.form['phone']
-      hashed_psw = bcrypt.generate_password_hash(psw).decode('utf-8')
-      cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-      cursor.execute("""
-        UPDATE userregister 
-        SET name=%s,gender=%s,dob=%s,phn=%s,addr=%s,bldgrp=%s,email=%s,psw=%s,ename=%s,relation=%s,phone=%s
-         WHERE Patient_id=%s""",(name,gender,dob,phn,addr,bldgrp,email,hashed_psw,ename,relation,phone,id))
-      mysql.connection.commit()
+
+      hashed_psw = generate_password_hash(psw, method='sha256')
+      user.psw=hashed_psw
+
+      db.session.commit()
+
+
       msg = 'You have successfully Updated your Details !'
-      row_details = display_details(session['phn'])
-      row_records = display_reports(session['id'])
+      row_details = display_details(user.phn)
+      row_records = display_reports(user.Patient_id)
       return render_template('index_user.html', msg=msg, rows=row_records, row1=row_details)
     return render_template('update.html')
 
@@ -245,6 +324,9 @@ def index_hospital():
     row_records = hospital_details(session['hid'])
     #print(row_records)
     return render_template('index_hospital.html', msg=msg, row=row_records)
+@app.route('/xyz', methods =['GET', 'POST'])
+def xyz():
+    return render_template('xyz.html')
 
 @app.route('/index_police', methods =['GET', 'POST'])
 def index_police():
@@ -266,46 +348,33 @@ def healthInsurance():
     return render_template('healthInsurance.html')
 
 def hospital_details(id):
-   #fetch reports of phn no. mentioned
-   cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-   cursor.execute("Select * from hospitalregister")
-   myresult = cursor.fetchall()  # fetching all rows of the table
-   for row in myresult:
-       #print(row)
-       if (id == row['hid']):  # if name is in row then return that row , row is a dictionary
-           return (row)
+           row= hospitalregister.query.filter_by(hid=id).all()# if name is in row then return that row , row is a dictionary
+           if(len(row)):
+             return (row[0])
 
 
 def police_details(id):
-   #fetch reports of phn no. mentioned
-   cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-   cursor.execute("Select * from policeregister")
-   myresult = cursor.fetchall()  # fetching all rows of the table
-   for row in myresult:
-       #print(row)
-       if (id == row['pid']):  # if name is in row then return that row , row is a dictionary
-           return (row)
+           row= policeregister.query.filter_by(pid=id).all()# if name is in row then return that row , row is a dictionary
+           if(len(row)):
+             return (row[0])
 
 def display_details(phn):
-   #fetch reports of phn no. mentioned
-   cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-   cursor.execute("Select * from userregister")
-   myresult = cursor.fetchall()  # fetching all rows of the table
-   for row in myresult:
-       #print(row)
-       if phn in row['phn']:  # if name is in row then return that row , row is a dictionary
-           return (row)
+           row = userregister.query.filter_by(phn=phn).all()
+           ###print(phn)
+           ###print(row[0])
+           if(len(row)):
+             return (row[0])
+           return render_template('UnregisteredUser.html')
 
 def display_reports(id):
     r=[]
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute("Select * from medical_records")
-    myresult = cursor.fetchall()  # fetching all rows of the table
+
+    myresult = medical_reports.query.filter_by(id=id).all() # fetching all rows of the table
     for row in myresult:
-        if (id == row['id']):# if name is in row then return that row
-            if(row['reports']==''):
-                row['reports']='https://drive.google.com/file/d/1oFkHtET8WNYl1S8wDsYBDqfjt_4u3_A-/view?usp=share_link'
-            r.append(row)
+        if (id == row.id):# if name is in row then return that row
+          
+            l={'Dname':row.Dname,'date':row.date,'sym':row.sym,'doc':row.doc,'allergies':row.allergies,'reports':row.reports}
+            r.append(l)
     return (r)
 
 
@@ -347,6 +416,7 @@ def train_model():
     labels = []
     userlist = os.listdir('static/faces')
     for user in userlist:
+      if os.path.exists(f'static/faces/{user}'):
         for imgname in os.listdir(f'static/faces/{user}'):
             img = cv2.imread(f'static/faces/{user}/{imgname}')
             resized_face = cv2.resize(img, (50, 50))
@@ -378,7 +448,7 @@ def start():
             identified_person = identify_face(face.reshape(1,-1))[0]
             print((identified_person))
             row_details=display_details(identified_person)
-            row_records=display_reports(row_details['Patient_id'])
+            row_records=display_reports(row_details.Patient_id)
             return render_template('hospital_display.html', row=row_details,row1=row_records)
 
         elif count>5:
@@ -403,7 +473,7 @@ def startp():
             identified_person = identify_face(face.reshape(1,-1))[0]
             row_details=display_details(identified_person)
             return render_template('police_display.html', row1=row_details)
-        elif count>3:
+        elif count>5:
             return render_template('UnregisteredUser.html')
 
 
@@ -443,4 +513,4 @@ def add():
 
 #### Our main function which runs the Flask App
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
