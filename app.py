a62e8fcf33dd3b0de6ef35532086528c97137121
Riddhi Shivhare <95924396+RiddhiShivhare@users.noreply.github.com>
2023-04-15 23:08:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -511,4 +511,4 @@ def add():
 
 #### Our main function which runs the Flask App
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
